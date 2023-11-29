42226e76d41d39d964b47069929b973919668fb5
JacksonLDJ <104440080+JacksonLDJ@users.noreply.github.com>
2023-11-29 22:58:24 +0000

    App_home
    Fixed access to app_home, no requires login - uses @login_required

--- ProbNet2/core/views.py
+++ ProbNet2/core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required
 def app_home(request):
     return render(request, 'core/app_home.html')
 
@@ -15,3 +17,7 @@ def contact(request):
 
 def login(request):        
         return render (request, 'core/login.html')
+
+def logout(request):
+     logout(request)
+     return "core/login.html"
