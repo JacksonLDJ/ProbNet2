d865d6da18a14644543136ae5f91ccb157de1b6b
JacksonLDJ <104440080+JacksonLDJ@users.noreply.github.com>
2023-11-29 23:34:24 +0000

    Logout
    Logout now works

--- ProbNet2/ProbNet2/urls.py
+++ ProbNet2/ProbNet2/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import app_home, contact
-from django.contrib.auth.views import LoginView
+from core.views import app_home, contact, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from core.forms import LoginForm
+
+
 
 
 urlpatterns = [
     path('app_home/', app_home, name='app_home'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('', LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='home', )
 ]
